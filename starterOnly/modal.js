97057e27209d2a5ba6b75d1d3a944a285fdef5a4
ybeb <younes.bouchi@gmail.com>
2024-03-27 14:13:44 +0100

    Fermeture de la modal avec la croix

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,29 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeX = document.querySelectorAll(".close")
+const inputFirst = document.getElementById("first");
+const inputLast = document.getElementById("last");
+const inputEmail = document.getElementById("email");
+const inputDate = document.getElementById("birthdate");
+const inputQuantity=document.getElementById("quantity");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
+closeX.forEach((btn)=>btn.addEventListener("click",closeModal))
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
 
+// close modal form 
+function closeModal(){
+modalbg.style.display="none";
+inputDate.value=" ";
+inputEmail.value=" ";
+inputLast.value=" ";
+inputFirst.value=" ";
+inputQuantity.value=" ";
+
+}
